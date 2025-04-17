17889bc1098da38aa375127aa2adc517e3fc421f
maxhta9 <max.hta9@gmail.com>
2025-04-16 22:08:55 -0600

    feat: hero home title

--- src/_core/constants/hero.const.ts
+++ src/_core/constants/hero.const.ts
@@ -1,4 +1,4 @@
-export const HOME_TITLE= "Transforming Your Business with Cutting-Edge Technology";
+export const HOME_TITLE= "Take the first step towards your digital evolution";
 export const HOME_DESCRIPTION= "Unlock the potential of your organization with our innovative technology solutions. Partner with us to streamline processes, enhance productivity, and drive growth.";
 export const SERVICE_TITLE= "Get ready to innovate";
 export const SERVICE_DESCRIPTION= "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
